bd816aa97a39c8f2c13850ae32970ca41c5f6036
giryong <giryong99@naver.com>
2022-04-24 00:33:49 +0900

    완성

--- src/musinsa/department/cgr/testPolynomial.java
+++ src/musinsa/department/cgr/testPolynomial.java
@@ -2,16 +2,26 @@ package musinsa.department.cgr;
 
 public class testPolynomial {
 	public static void main(String args[]) {
-		int [] a = new int[] {4,3,5,0};
-		int [] b = new int[] {3,1,0,1,2};
-			Polynomial A = new Polynomial(3,a);
-			Polynomial B = new Polynomial(4,b);
-			Polynomial C = A.addPoly(B);
+		int [] a = new int[] {3, 7 , 10 , 4, -5 ,0};
+		int [] b = new int[] {1, 1, 1, 1};
+			Polynomial A = new Polynomial(a);
+			Polynomial B = new Polynomial(b);
+			Polynomial C = A.add(B);
 			
 			
-		System.out.printf("A(x)=" + A + "\n");
-		System.out.printf("B(x)=" + B);
-		System.out.printf("\nC(x)=" + C);
+			Polynomial f1 = new Polynomial(a);
+			Polynomial f2 = new Polynomial(b);
+			Polynomial f3 = new Polynomial(2);
+			
+			System.out.printf("f1(x) = " + f1);
+			System.out.printf("f2(x) = " + f2);
+			f2 = f2.neg();
+			System.out.printf("f2(x) = " + f2);
+			System.out.printf("f3(x) = " + f3);
+
+			System.out.printf("f1(x) + f2(x)  = " + f1.add(f2));
+			System.out.printf("f2(x) - f1(x)  = " + f2.sub(f1));
+			System.out.printf("f2(x) + -f1(x) = " + f2.add(f1.neg()));
 	}
 
 }
